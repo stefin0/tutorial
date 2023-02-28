044f9c5398220937180a253c2135fd94075f24fe
stefin <stefinracho@gmail.com>
2023-02-27 21:51:03 -0800

    🧪Test

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="text-center mt-16">
+    <main>
       <Navbar />
     </main>
   );
